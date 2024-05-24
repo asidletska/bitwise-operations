68f720d60ca8c341e98aacf264aceb90b0f01759
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-24 15:36:23 +0300

    Create repository

---
+++ Fan (bitwise operators)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        // We have a fan with 1 byte of RAM
+        // Let's imagine that we need to fit this code into 1 byte
+        // 0000 0000
+        // ХХХХ ХХХ0 -> The fan is off
+        // ХХХХ ХХХ1 -> The fan is on
+        // ХХХХ ХХ0Х -> The fan is not spinning
+        // ХХХХ ХХ1Х -> The fan is spinning
+        // ХХХХ 01ХХ -> Low speed
+        // ХХХХ 10ХХ -> Normal speed
+        // ХХХХ 11ХХ -> High speed
+
+
+        // With switch:
+
+        byte Settings = 0b00000000;
+        bool isFanOn = true;
+        bool isFanRotating = true;
+        int fanSpeed = 3; // 1 - low, 2 - normal, 3 - high
+
+        if (isFanOn)
+        {
+            Console.WriteLine("the power indicator light came on");
+            if (isFanRotating)
+            {
+                Console.WriteLine("the fan spins");
+            }
+            switch (fanSpeed)
+            {
+                case 1:
+                    Console.WriteLine("the fan rotates at low speed");
+                    break;
+                case 2:
+                    Console.WriteLine("the fan spins at normal speed");
+                    break;
+                case 3:
+                    Console.WriteLine("the fan spins at high speed");
+                    break;
+                default:
+                    Console.WriteLine("wrong speed value");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ With constant/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        // With constant:
+
+        byte Settings1 = 0b00001011;
+        const byte FAN_ON = 0b00000001;
+        const byte FAN_ROTATING = 0b00000010;
+        const byte LOW_SPEED = 1;
+        const byte NORMAL_SPEED = 2;
+        const byte HIGH_SPEED = 3;
+
+        // i use bitwise right shift because we need to shift the bits that determine the speed 
+        //                 0000 [00]00
+        // Low speed    -> 0000 [01]00 >>2 -> 0000 0001 -> 1
+        // Normal speed -> 0000 [10]00 >>2 -> 0000 0010 -> 2
+        // High speed   -> 0000 [11]00 >>2 -> 0000 0011 -> 3
+
+        if ((Settings1 & FAN_ON) == FAN_ON) // brackets to increase the priority
+        {
+            Console.WriteLine("the power indicator light came on");
+
+            if ((Settings1 & FAN_ROTATING) == FAN_ROTATING)
+            {
+                Console.WriteLine("the fan spins");
+            }
+
+            if ((Settings1 >> 2) == LOW_SPEED) 
+            {
+                Console.WriteLine("the fan rotates at low speed");
+            }
+            else if ((Settings1 >> 2) == NORMAL_SPEED)
+            {
+                Console.WriteLine("the fan spins at normal speed");
+            }
+            else if ((Settings1 >> 2) == HIGH_SPEED)
+            {
+                Console.WriteLine("the fan spins at high speed");
+            }
+            else
+            {
+                Console.WriteLine("wrong speed value");
+            }
+        }
+    }
+}
+
